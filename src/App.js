0f586b8aa2fbe67c9fbe83998e01af53817d45a7
Hellowizz <heloroussel@hotmail.fr>
2021-02-21 19:46:05 +0100

    new design, text animation, test API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 /* COMPONENTS IMPORT */
 import Bubble from './components/Bubble.js';
 import RightSideGame from './components/RightSideGame.js';
 
+function handleTest (searchTerm) {
+	fetch(`https://api.themoviedb.org/3/person/popular?api_key=5d9a27da8576bf54dda76fd8ef6ebebb&language=en-US&page=1`)
+	.then(data => data.json())
+	.then(data => {
+		console.log('datas : ' + data.results);
+	})
+}
+
 function App() {
-  return (
-    <div className="App">
-      <div className="game-container">
-        <Bubble />
-        <RightSideGame />
-      </div>
-      <div className="background-image-container"/>
-    </div>
-  );
+
+  	return (
+	    <div className="App">
+	      <div className="game-container">
+	      	{ handleTest() }
+	        <Bubble />
+	        <RightSideGame />
+	      </div>
+	      <div className="background-image-container" />
+	    </div>
+ 	 );
 }
 
 export default App;
--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,21 +1,89 @@
+import React, { useState } from 'react';
 import './Bubble.css';
 
 /* IMAGES */
 import Pate from '../img/pate.svg';
 
-let fullText = new Array(
-	"<h1>WELCOME !</h1>", 
-	"<p>My name is Pierre-Jean, I'm the presentator</p>",
-	"<p>of this amazing game<p>"
-);
+/* MATERIAL UI */
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+/* TYPERITTER */
+import Typewriter from 'typewriter-effect';
+
+/* BUTTON CUSTOMISATION */
+const CustomButton = withStyles({
+  root: {
+    backgroundColor: '#f89141',
+    '&:hover': {
+      backgroundColor: '#89ee97'
+    }
+  },
+  text: {
+    padding: '15px 30px',
+    color: 'white',
+    fontWeight: '600',
+    fontFamilu: 'Trebuchet MS, sans-serif'
+  }
+})(Button);
 
 function Bubble() {
+  const [phaseGame, setPhaseGame] = useState('welcome1');
+
   return (
     <div className="bubble-container">
-    	<div className="bubble-in">
-    		<div className="typewriter">{fullText}</div>
-    		<div className="bubble-pate-container"><img alt="Pate" src={Pate} width='150px'/></div>
-    	</div>
+      	{ phaseGame === 'welcome1' &&
+          <div className="bubble-in">
+        		<div className="typewriter">
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .changeDelay('50')
+                    .typeString('<h1>WELCOME !</h1>')
+                    .changeDelay('20')
+                    .typeString('<p>My name is Pierre-Jean, I\'m the presentator</p>')
+                    .typeString('<p>of this amazing game<p>')
+                    .changeDelay('50')
+                    .pauseFor()
+                    .typeString('<h2 className="title-game">HELLO MOVIE KWIZZ !</h2>')
+                    .start();
+                }}
+              />
+            </div>
+            <div className="bubble-buttons">
+              <CustomButton onClick={() => setPhaseGame('welcome2')}>Okay</CustomButton>
+            </div>
+          </div>
+        }
+
+        {/* CREER UNE FONC POUR EVITER REPETITION */}
+
+        { phaseGame === 'welcome2' &&
+          <div className="bubble-in">
+            <div className="typewriter">
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .changeDelay('20')
+                    .typeString('<p>The rules are simples,</p>')
+                    .typeString('<p>I\'m going to ask you questions about movies<p>')
+                    .typeString('<p>and you will have <span>one minute</span> to answer,<p>')
+                    .changeDelay('50')
+                    .pauseFor()
+                    .typeString('<h2 className="title-game">LET\'S HAVE FUN !</h2>')
+                    .start();
+                }}
+              />
+            </div>
+            <div className="bubble-buttons">
+              <CustomButton onClick={() => setPhaseGame('game')}>Play</CustomButton>
+            </div>
+          </div>
+        }
+
+        { phaseGame === 'game' &&
+          <div className="bubble-in" />
+        }
     	<div className="bubble-under"/>
     </div>
   );
